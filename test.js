bc8611bbd66e038100b43318c80e3f17962eef9f
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2023-09-13 18:54:14 +0400

    100% tests coverage

--- test.js
+++ test.js
@@ -4,6 +4,13 @@ import mediasoupNodeTests from '@mafalda-sfu/mediasoup-node-tests'
 import RemoteMediasoupClientMock from '@mafalda-sfu/remote-mediasoup-client-mock'
 
 
+// eslint-disable-next-line jest/no-hooks
+afterAll(function()
+{
+  remoteMediasoupClientMock.close()
+})
+
+
 test('layout', function()
 {
   expect.assertions(1)
@@ -11,8 +18,34 @@ test('layout', function()
   expect(RemoteMediasoupClientMock).toMatchInlineSnapshot('[Function]')
 })
 
+test('lifecycle', async function()
+{
+  expect.assertions(8)
 
-const remoteMediasoupClientMock = new RemoteMediasoupClientMock(
+  remoteMediasoupClientMock = new RemoteMediasoupClientMock({WebSocket: {}})
+
+  expect(remoteMediasoupClientMock.mediasoup).toBeUndefined()
+  expect(remoteMediasoupClientMock.readyState).toBe(3)
+
+  remoteMediasoupClientMock.open('ws://example.com')
+
+  expect(remoteMediasoupClientMock.mediasoup).toBeUndefined()
+  expect(remoteMediasoupClientMock.readyState).toBe(1)
+
+  await once(remoteMediasoupClientMock, 'connected')
+
+  expect(remoteMediasoupClientMock.mediasoup).toBeDefined()
+  expect(remoteMediasoupClientMock.readyState).toBe(4)
+
+  remoteMediasoupClientMock.close()
+
+  expect(remoteMediasoupClientMock.mediasoup).toBeUndefined()
+  expect(remoteMediasoupClientMock.readyState).toBe(3)
+})
+
+
+// eslint-disable-next-line jest/require-hook
+let remoteMediasoupClientMock = new RemoteMediasoupClientMock(
   'ws://example.com'
 )
 
