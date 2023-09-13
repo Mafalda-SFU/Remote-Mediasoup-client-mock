ace389f030f92ac56f1ddac85d4b92ecdb9bfcbb
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2023-09-13 07:49:56 +0400

    Use Mediasoup Node.js tests

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
-import {deepStrictEqual} from 'node:assert/strict'
-import {test} from 'node:test'
+import {once} from 'node:events';
 
+import mediasoupNodeTests from '@mafalda-sfu/mediasoup-node-tests'
 import RemoteMediasoupClientMock from '@mafalda-sfu/remote-mediasoup-client-mock'
 
 
 test('layout', function()
 {
-  deepStrictEqual(typeof RemoteMediasoupClientMock, 'function')
+  expect.assertions(1)
+
+  expect(RemoteMediasoupClientMock).toMatchInlineSnapshot('[Function]')
 })
+
+
+const remoteMediasoupClientMock = new RemoteMediasoupClientMock('ws://example.com')
+
+await once(remoteMediasoupClientMock, 'connected')
+
+// eslint-disable-next-line jest/require-hook
+mediasoupNodeTests(remoteMediasoupClientMock.mediasoup)
